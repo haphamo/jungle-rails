e9682f207c7d6a74a4ad3c6217403c499f2abe48
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-14 20:19:24 -0500

    completed about us page

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+  resources :about, only: [:index]
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
+
   end
 
   resources :orders, only: [:create, :show]
