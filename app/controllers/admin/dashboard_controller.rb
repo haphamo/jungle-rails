9aaf746068d9387e98f80f9d40a3ae228a0499cd
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-14 19:25:28 -0500

    Dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @number_of_products = Product.count
+    @number_of_categories = Category.count
   end
 end
+
+# Display a count of how many products are in the database
+# Display a count of how many categories are in the database
